748950675337030f3287c4a1e7d05e7cf8686206
Charles Boudrant <charlesboudrant@protonmail.com>
2025-05-13 09:38:22 +0200

    Remove content layer integration from Next.js config and delete outdated update documents.

---
+++ content/fetch.ts
@@ -0,0 +1,16 @@
+export const contentGqlFetcher = async (query: string, variables = {}, preview = false) => {
+  const res = await fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${
+        preview ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN : process.env.CONTENTFUL_ACCESS_TOKEN
+      }`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query, variables }),
+  })
+
+  const { data, errors } = await res.json()
+  if (errors) throw new Error('Could not get content from Contentful')
+  return data
+}
